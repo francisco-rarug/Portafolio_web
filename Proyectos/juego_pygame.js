1fa2d995722e0e9823e27855e178f1b71029e467
Rarug <frarug@provart.com.ar>
2023-08-11 16:17:27 -0300

    WIP mejoras en la galeria del proyecto 2 y boton agregado para ver video del proyecto

--- Proyectos/juego_pygame.js
+++ Proyectos/juego_pygame.js
@@ -1,10 +1,36 @@
 function ampliarImagen(id) {
     var imagen = document.getElementById(id);
-    imagen.style.width = "600px";
-    imagen.style.height = "400px";
+    
+    if (imagen.getAttribute('data-ampliado') == 'no') {
+        
+        imagen.style.width = "600px";
+        imagen.style.height = "400px";
+        
+        imagen.setAttribute('data-ampliado', 'si');
+    } else {
+        
+        imagen.style.width = "300px";
+        imagen.style.height = "200px";
+        
+        imagen.setAttribute('data-ampliado', 'no');
+    }
 
     setTimeout(function() {
         imagen.style.width = "300px";
         imagen.style.height = "200px";
     }, 5000);
 }
+
+
+var showVideoBtn = document.getElementById('showVideoBtn');
+showVideoBtn.addEventListener('click', function() {
+    var videoContainer = document.getElementById('videoContainer');
+    
+    if (videoContainer.style.display === "none" || videoContainer.style.display === "") {
+        videoContainer.style.display = "block";
+        showVideoBtn.innerText = "Ocultar Video";  
+    } else {
+        videoContainer.style.display = "none";
+        showVideoBtn.innerText = "Ver Video en youtube"; 
+    }
+});
